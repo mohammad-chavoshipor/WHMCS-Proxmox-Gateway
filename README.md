3f08497a26e1f6fddad6451843ef346667a35aa6
Baptiste <baptiste62100@gmail.com>
2019-03-31 15:47:13 +0200

    Adding requirements for novnc console

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Because fuck modulegarden.
 
 Requirements : WHMCS 7+, Proxmox 5+
-Installation : Just ulpoad the module onto your whmcs folder and enable the module.
+Installation : 
+* Step 1 : Ulpoad the module onto your whmcs folder and enable the module. 
+* Step 2 : Upload the proxmox folder content into `/usr/share/novnc-pve` (on your promox server obviously)
 
 New features : Juste open an issue if you need something.
---
+++ promox/app/mgui.js
@@ -0,0 +1,71 @@
+/*
+ * noVNC: HTML5 VNC client
+ * Copyright (C) 2012 Joel Martin
+ * Copyright (C) 2015 Samuel Mannehed for Cendio AB
+ * Licensed under MPL 2.0 (see LICENSE.txt)
+ *
+ * See README.md for usage and integration instructions.
+ */
+
+/* jslint white: false, browser: true */
+/* global window, $D, Util, WebUtil, RFB, Display */
+var UI;
+
+(function () {
+    "use strict";
+
+    // Load supporting scripts
+    window.onscriptsload = function () {
+        UI.load();
+    };
+    Util.load_scripts(["webutil.js"]);
+    
+    UI = {
+        load: function (callback) {
+            var token = WebUtil.getQueryVar('token', null);
+            if (token) {
+                WebUtil.createCookie('PVEAuthCookie', token, 1);
+            }
+            var error = WebUtil.getQueryVar('error', null);
+            if (error && error !== undefined && error !== null) {
+                UI.updateState(null, 'failed', 'loaded',
+                        error);
+                return;
+            }
+            var consoleType = "";
+            if (WebUtil.getQueryVar('virtualization') == "qemu") {
+                consoleType = "kvm";
+            } else
+                consoleType = WebUtil.getQueryVar('virtualization');
+            window.location = "../?console=" + consoleType + "&novnc=1&vmid=" + WebUtil.getQueryVar('vmid') + "&vmname=" + WebUtil.getQueryVar('vmid') + "&node=" + WebUtil.getQueryVar('node') + "";
+        },
+        updateState: function (rfb, state, oldstate, msg) {
+            var klass;
+            switch (state) {
+                case 'failed':
+                case 'fatal':
+                    klass = "noVNC_status_error";
+                    break;
+                case 'normal':
+                    klass = "noVNC_status_normal";
+                    UI.pveAllowMigratedVMTest = true;
+                    break;
+                case 'disconnected':
+                    $D('noVNC_logo').style.display = "block";
+                    $D('noVNC_container').style.display = "none";
+                    /* falls through */
+                case 'loaded':
+                    klass = "noVNC_status_normal";
+                    break;
+                default:
+                    klass = "noVNC_status_warn";
+                    break;
+            }
+            if (typeof (msg) !== 'undefined') {
+                $D('noVNC-control-bar').setAttribute("class", klass);
+                $D('noVNC_status').innerHTML = msg;
+            }
+
+        },
+    };
+})();
